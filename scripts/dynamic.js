aac68f122eec6b04bb13bb21045d913353c48619
rhettkrawitt <144711857+rhettkrawitt@users.noreply.github.com>
2024-05-03 16:08:09 +0000

    dynamic content assignemnt

---
+++ scripts/dynamic.js
@@ -0,0 +1,12 @@
+let colors = ["red", "green", "lime", "navy", "blue", "aqua", "azure"];
+
+let ul = document.createElement("ul");
+
+for (i = 0; i < colors.length; i++) {
+    let li = document.createElement("li");
+    li.append(colors[i]);
+    li.style.color = colors[i]
+    ul.appendChild(li);
+}
+
+document.body.appendChild(ul);
