61ca9db01451e170cd2382b09cf07360834b9459
rhettkrawitt <144711857+rhettkrawitt@users.noreply.github.com>
2024-05-24 17:00:18 +0000

    add img6

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,6 +1,6 @@
 gallery = document.getElementById("gallery")
 
-for (let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 6; i++) {
     let img = document.createElement("img")
     img.src = "images/gallery-images/" + i + ".JPG"
     gallery.appendChild(img)
