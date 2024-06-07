01f7ad6f35b4c9ed66e040ba10954c7c166caec2
rhettkrawitt <144711857+rhettkrawitt@users.noreply.github.com>
2024-06-07 00:23:34 +0000

    coaches page

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,5 +3,6 @@ gallery = document.getElementById("gallery")
 for (let i = 1; i <= 6; i++) {
     let img = document.createElement("img")
     img.src = "images/gallery-images/" + i + ".JPG"
-    gallery.appendChild(img)
+    img.alt = "Redwood Sailors Sailing and at Practice"
+    gallery.appendChild(img);
 }
